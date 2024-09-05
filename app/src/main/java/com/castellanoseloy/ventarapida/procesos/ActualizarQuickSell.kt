1c61ebfde24860cafa6248b62ba4288aeb907903
el_pyro <castellanoseloy@gmail.com>
2024-09-05 12:02:17 -0500

    antes de cambio de paquete

--- app/src/main/java/com/castellanoseloy/ventarapida/procesos/ActualizarQuickSell.kt
+++ app/src/main/java/com/castellanoseloy/ventarapida/procesos/ActualizarQuickSell.kt
@@ -17,48 +17,48 @@ class ActualizarQuickSell(private val sku: String, private val quantity: Int) {
     private val gson = Gson()
 
     fun updateInventory() {
-        CoroutineScope(Dispatchers.IO).launch {
-            try {
-                // Crear el objeto de actualización
-                val update = ModeloActualizadorQuickSell(sku, quantity)
-                val updateRequest = UpdateRequest(listOf(update))
-
-                // Convertir el objeto a JSON
-                val json = gson.toJson(updateRequest)
-
-                // Crear el cuerpo de la solicitud
-                val body =
-                    RequestBody.create("application/json; charset=utf-8".toMediaTypeOrNull(), json)
-
-                // Construir la solicitud
-                val request = Request.Builder()
-                    .url("https://merchant.api.quicksell.co/public/v1/m/inventory/skus")
-                    .put(body)
-                    .addHeader("accept", "application/json")
-                    .addHeader("content-type", "application/json")
-                    .addHeader(
-                        "x-api-key",
-                        "ZzuHkQm32INQ/ZU9G5nbF4nIQGC6AoFq5T952tvp349dM3CgBLEkVj4L5lGvnAZP"
-                    )
-                    .build()
-
-                client.newCall(request).execute().use { response ->  // Aquí se asegura el cierre del ResponseBody
-                    withContext(Dispatchers.Main) {
-                        if (response.isSuccessful) {
-                            // Manejar éxito (e.g., mostrar un mensaje de éxito)
-                            Log.i("QuickSell","Inventario quicksell actualizado con éxito de: $sku , cantidad: $quantity")
-                        } else {
-                            // Manejar error (e.g., mostrar un mensaje de error)
-                            Log.i("QuickSell","Error en la actualización del inventario:  de: $sku con el código de respuesta: ${response.code}")
-                        }
-                    }
-                }
-            } catch (e: IOException) {
-                withContext(Dispatchers.Main) {
-                    // Manejar error (e.g., mostrar un mensaje de error)
-                    e.printStackTrace()
-                }
-            }
-        }
+//        CoroutineScope(Dispatchers.IO).launch {
+//            try {
+//                // Crear el objeto de actualización
+//                val update = ModeloActualizadorQuickSell(sku, quantity)
+//                val updateRequest = UpdateRequest(listOf(update))
+//
+//                // Convertir el objeto a JSON
+//                val json = gson.toJson(updateRequest)
+//
+//                // Crear el cuerpo de la solicitud
+//                val body =
+//                    RequestBody.create("application/json; charset=utf-8".toMediaTypeOrNull(), json)
+//
+//                // Construir la solicitud
+//                val request = Request.Builder()
+//                    .url("https://merchant.api.quicksell.co/public/v1/m/inventory/skus")
+//                    .put(body)
+//                    .addHeader("accept", "application/json")
+//                    .addHeader("content-type", "application/json")
+//                    .addHeader(
+//                        "x-api-key",
+//                        "ZzuHkQm32INQ/ZU9G5nbF4nIQGC6AoFq5T952tvp349dM3CgBLEkVj4L5lGvnAZP"
+//                    )
+//                    .build()
+//
+//                client.newCall(request).execute().use { response ->  // Aquí se asegura el cierre del ResponseBody
+//                    withContext(Dispatchers.Main) {
+//                        if (response.isSuccessful) {
+//                            // Manejar éxito (e.g., mostrar un mensaje de éxito)
+//                            Log.i("QuickSell","Inventario quicksell actualizado con éxito de: $sku , cantidad: $quantity")
+//                        } else {
+//                            // Manejar error (e.g., mostrar un mensaje de error)
+//                            Log.i("QuickSell","Error en la actualización del inventario:  de: $sku con el código de respuesta: ${response.code}")
+//                        }
+//                    }
+//                }
+//            } catch (e: IOException) {
+//                withContext(Dispatchers.Main) {
+//                    // Manejar error (e.g., mostrar un mensaje de error)
+//                    e.printStackTrace()
+//                }
+//            }
+//        }
     }
 }
